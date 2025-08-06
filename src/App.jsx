a371148854390838cb6773538a8196c24cbc18fd
adduup <adwait.uprety21@gmail.com>
2025-08-06 12:32:28 -0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="center-container">
   <img
-    src="/public/addyup.png"
+    src="/addyup.png"
     alt="Centered"
     className="centered-image"
   />
