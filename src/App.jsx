4cc0bf4b5bf1341d253613d947d788abf12f85cb
adduup <adwait.uprety21@gmail.com>
2025-08-06 12:45:55 -0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import addylogo from './assets/addyup.png'
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="center-container">
-  <img
-    src={addylogo}
-    alt="Centered"
-    className="centered-image"
-  />
-</div>
+   <div className="center-container">
+      <img
+        src={addylogo}
+        alt="Centered"
+        className="centered-image"
+      />
+    </div>
     
   )
 }
