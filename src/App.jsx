64231c15d314fa64831e57065077d97f4fff1d16
adduup <adwait.uprety21@gmail.com>
2026-02-05 11:47:23 -0600

    mac

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,6 @@ function App() {
           
         </div>
       </section>
-<a href="/assets/downloads/RocketUI-ARM64.zip" download>
-  Download RocketUI (ARM64)
-</a>
-<p>Note: This version is for Apple Silicon (ARM64) Macs only.</p>
 
       
     </div>
